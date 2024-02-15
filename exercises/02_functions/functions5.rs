9659f4e2ce9f40a4e23620d9eed2df40016fc90e
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-16 00:09:42 +0100

    Mark 02_5 done

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let question = 5;
     let answer = square(question);
